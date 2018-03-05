06bb3edc6523b2a51ff07112861fc7f807f3af96
amanvir <hello@amanvir.io>
2018-03-05 00:07:11 +0000

    add test to check for eval

--- test/test.js
+++ test/test.js
@@ -2,9 +2,29 @@ import test from 'ava'
 import Mental from '../src/mental.js'
 
 test.beforeEach(t => {
-  t.context.cw = new Cryptowatch()
+  t.context.m = new Mental()
 })
 
-test('test', t => {
-  t.is(2,2)
+test('it finds usages of eval', t => {
+
+  //arrange
+  const javascript = `
+    const x = '1+1';
+    eval(x);
+    let y = 222;
+  `
+  
+  const query = '//eval';
+
+  //act 
+  const output = t.context.m.search(javascript, query)
+  
+  //expected
+  const expected = { 
+    lineNumber: 2
+    code: 'eval(x)'
+  }
+
+  //assert
+  t.is(output, expected)
 })
