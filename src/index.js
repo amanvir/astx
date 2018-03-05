eebabd2f13fd1a011dbee02c4e54e3a26f57dc24
amanvir <hello@amanvir.io>
2018-03-05 00:43:33 +0000

    Initial template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 class Mental {
-  constructor (options = {}) {
+  constructor (src) {
+    this.src = src
+    this.query = query
+  }
+
+  parse (code) {
+    // set up parser that parses the AST
+  }
+
+  convertToXML (parsedJSON) {
+    // convert the result into XML
+  }
+
+  query (xml, query) {
+  }
+
+  search (code, query) {
+    const parsedCode = this.parse(code)
+    const xml = this.converToXML(parsedCode)
+    const queryResult = this.query(xml, query)
+    return this.formatResult(queryResult)
+  }
+
+  formatResult (queryResult) {
   }
 }
 
